4795f79ab0a8eb309722bd5c4d789c70585ba238
gamped <mathiasgam@gmail.com>
2018-11-05 15:21:21 +0100

    Added login UI

---
+++ Frontend/p3-warehouse/src/adminIndex.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './adminIndex.css';
+import './style.css';
+
+// The Menu component
+class Menu extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            homeText: "Home",
+            ordersText: "Orders",
+            clientsText: "Clients",
+            stockText: "Stock",
+        };
+    }
+
+    render(){
+        let home = this.state.homeText;
+        let orders = this.state.ordersText;
+        let clients = this.state.clientsText;
+        let stock = this.state.stockText;
+
+        return(
+            <div class="menuStyle">
+                <button>{home}</button>
+                <button>{orders}</button>
+                <button>{clients}</button>
+                <button>{stock}</button>
+            </div>
+        );
+    }
+}
+
+// Send components to HTML
+ReactDOM.render(
+    <div>
+        <Menu />
+    </div>
+    , document.getElementById('root')   
+);
--- Frontend/p3-warehouse/src/index.js
+++ Frontend/p3-warehouse/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
-
+import './style.css';
 
 // The header component
 class Header extends React.Component{
@@ -24,17 +23,40 @@ class Header extends React.Component{
     }
 }
 
+// The box for sign-in to the system
+class SignInBox extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: "",
+            username: "",
+        };
+    }
+    
+    render(){
+        let signInText = this.state.signText;
+        return(
+            <div>
+                <div class="signBox">
+                    <img src={require('./resources/4n_logo_mini.jpg')} class="logoPic"/> 
+                    <input type="Username" placeholder="Username" ></input>
+                    <input type="Password" placeholder="Password" ></input>
+                    <button class="signButton">Sign in</button>
+                </div>
+                    <div class="textureBox"> 
+                    <a href="https://www.freepik.com/free-vector/book-shelves-dtcorative-colorful-icon-poster_2871137.htm">Image designed by Macrovector</a>
+                </div>
+            </div>
+        );
+    }
+}
 
 // Send components to HTML
 ReactDOM.render(
     <div>
         <Header />
+        <SignInBox />
     </div>
     , document.getElementById('root')   
 );
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
