9bee9916a1fbc7cc82c48f8d4b62959b757c578a
Tobias <nielstns@gmail.com>
2018-11-09 21:42:34 +0100

    Played around a little.
    Mostly just added a couple of methods in index.js

--- Frontend/p3-warehouse/src/index.js
+++ Frontend/p3-warehouse/src/index.js
@@ -36,15 +36,33 @@ class SignInBox extends React.Component{
         };
     }
 
+    // Updates the username part of the state
+    emailTypedHandler = (event) =>{
+      this.setState({
+        username: event.target.value
+      })
+    }
+
+    // Updates the password part of the state
+    passwordTypedHandler = (event)=>{
+      this.setState({
+        password: event.target.value
+      })
+    }
+
+    logTheStateHandler = () => {
+      console.log(this.state)
+    }
+
     render(){
         let signInText = this.state.signText;
         return(
             <div class="signBox">
-                <img src={require('./resources/4n_logo_mini.jpg')} class="logoPic"/>
-                <input type="Email" placeholder="Email" ></input>
-                <input type="Password" placeholder="Password" ></input>
+                <img src={require('./resources/4n_logo_mini.jpg')} class="logoPic" alt="The logo of 4N"/>
+                <input type="Email" placeholder="Email" onChange={this.emailTypedHandler}></input>
+                <input type="Password" placeholder="Password" onChange={this.passwordTypedHandler}></input>
                 <form action="/login">
-                    <button onClick={console.log("Sign in has been pressed")} class="signButton" >Sign in</button>
+                    <button onClick={this.logTheStateHandler} class="signButton" >Sign in</button>
                 </form>
             </div>
         );
