64576548d163d84280e5437099e408d1907946f4
Tobias <nielstns@gmail.com>
2018-11-13 14:49:52 +0100

    Update Frontend/p3-warehouse/src/index.js

--- Frontend/p3-warehouse/src/index.js
+++ Frontend/p3-warehouse/src/index.js
@@ -15,20 +15,12 @@ import AdminOrders from './components/pages/orders/AdminOrders';
 ReactDOM.render(
     <BrowserRouter>
         <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Route exact path="/" component={Header}/>
         <Route exact path="/" component={SignInBox}/>        
         <Route path= "/AdminIndex" component={AdminIndex}/>
         <Route path = "/components/pages/orders/AdminOrders" component={AdminOrders} />
-=======
-            <Route exact path="/" component={Header}/>
-=======
->>>>>>> cb5be6777005fc707ff408cc9cee7ac0c34daa75
             <Route exact path="/" component={SignInBox}/>        
             <Route path= "/AdminIndex" component={AdminIndex}/>
             <Route path= "/pages/orders/AdminOrders" component={AdminOrders}/>
->>>>>>> ac9353b4adb83cbffe58f8088aa8ace88aceb370
         </div>
     </BrowserRouter>
     , document.getElementById('root')
