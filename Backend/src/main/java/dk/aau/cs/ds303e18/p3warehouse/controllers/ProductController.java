80ee836282ae55370d579170e2bd924390f251d4
simonmartinnielsen <post.smn@gmail.com>
2018-11-14 17:37:55 +0100

    Just a comment

--- Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/controllers/ProductController.java
+++ Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/controllers/ProductController.java
@@ -31,6 +31,8 @@ public class ProductController {
     }
     @PutMapping("/products/{id}")
     Product replaceProduct(@RequestBody Product newProduct, @PathVariable String id){
+
+        // Conditionals to check for null
         return productRepository.findById(id)
                 .map(product -> {
                     product.setName(newProduct.getName());
