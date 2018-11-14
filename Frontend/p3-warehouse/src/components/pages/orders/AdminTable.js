6b0c16c4ca6b4c4ab739b75d0175a7c7d807d7b0
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2018-11-14 15:03:53 +0100

    Slight table changes

--- Frontend/p3-warehouse/src/components/pages/orders/AdminTable.js
+++ Frontend/p3-warehouse/src/components/pages/orders/AdminTable.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactTable from "react-table"
 import 'react-table/react-table.css'
 import "./orders.css"
-import Table from '../../MenuComponents/Table';
 
 class AdminTable extends React.Component {
     constructor(props) {
@@ -15,18 +14,18 @@ class AdminTable extends React.Component {
 
     render() { 
         const {data} = this.state.data;
-        var rTable = new Table()
-        rTable.state.data = this.state.data
-        rTable.state.columns = [
-            {Header: "Package Name", accessor: "name"},
-            {Header: "Package ID", accessor: "id"},
-            {Header: "Unit Amount", accessor: "amount"},
-            {Header: "Unit Type", accessor: "type"},
-            {Header: "Packaged?", accessor: "packaged"}
-        ]
         return ( 
             <div className = "AdminTable">
-                <rTable />
+                <ReactTable
+                    data = {data}
+                    columns = {[
+                        {Header: "Package Name", accessor: "name"},
+                        {Header: "Package ID", accessor: "id"},
+                        {Header: "Unit Amount", accessor: "amount"},
+                        {Header: "Unit Type", accessor: "type"},
+                        {Header: "Packaged?", accessor: "packaged"}
+                    ]}
+                />
                 <button>Mark All Packed</button>
                 <button>Print Pack List</button>
                 <button>Edit Order</button>
