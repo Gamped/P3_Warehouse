6d8bc79fb9ce84e93c599f8a9b123a3b5c71bac1
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2018-11-18 13:41:52 +0100

    Table up and running

--- Frontend/p3-warehouse/src/components/pages/Admin/orders/AdminOrders.js
+++ Frontend/p3-warehouse/src/components/pages/Admin/orders/AdminOrders.js
@@ -2,19 +2,24 @@ import React from 'react';
 import Table from "../../../MenuComponents/Table"
 import "../../Pages.css";
 
+//TODO: Implement data fetching from backend/database
 const AdminOrders = (props) => {
     const columns = [
-        {Header: "Product Name", accessor: "name"},
-        
+        {Header: "Package Name", accessor: "name"},
+        {Header: "Package ID", accessor: "id"},
+        {Header: "Unit Amount", accessor: "amount"},
+        {Header: "Unit Type", accessor: "type"},
+        {Header: "Packaged?", accessor: "packaged"}
     ]
 
     return(
         <div className="PageStyle">
             <customText_b>You are on Admin order page</customText_b>
+            <Table columns={columns}/>
         </div>
 
     );
 }
- 
+
 export default AdminOrders;
 
