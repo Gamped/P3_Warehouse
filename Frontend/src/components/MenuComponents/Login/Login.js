181b2b4578e7dfde8a04817284cbfef2444b4f5a
Tobias <nielstns@gmail.com>
2018-11-22 11:03:59 +0100

    Made it so that there is only one button
    You are still able to change where you wanna login, depending on input.

--- Frontend/src/components/MenuComponents/Login/Login.js
+++ Frontend/src/components/MenuComponents/Login/Login.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import "./Login.css";
+import {withRouter} from "react-router-dom";
 
 // The box for sign-in to the system
-export default class SignInBox extends React.Component{
+class SignInBox extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
@@ -27,11 +28,19 @@ export default class SignInBox extends React.Component{
 
     // Logs the state
     logTheStateHandler = (event) => {
-        //event.preventDefault()
-        if (this.state.username.toLowerCase ==="admin"){ //Temp work until connected to backend
-            
+        event.preventDefault()
+        if (this.state.username.toLowerCase() ==="admin"){ //Temp work until connected to backend
+            console.log("WTF I SHOULD BE CHANGING TO ADMIN")
+            this.props.history.push("./Admin")
+        } else if(this.state.username.toLowerCase() ==="user"){
+            this.props.history.push("./User")
+            console.log("WTF I SHOULD BE CHANGING TO USER")
+        } else{
+            alert("Email should be either: User or Admin")
         }
       console.log(this.state)
+      console.log(this.props)
+      
     }
 
     render(){
@@ -40,16 +49,15 @@ export default class SignInBox extends React.Component{
             <div>
                 <div className="signBox">
                     <img src={require('../../../resources/4n_logo_mini.jpg')} className="logoPic" alt="The logo of 4N"/>
-                    <input type="Email" placeholder="Email" onChange={this.emailTypedHandler}></input>
-                    <input type="Password" placeholder="Password" onChange={this.passwordTypedHandler}></input> 
-                    <form action="./Admin">
-                        <button onClick={this.logTheStateHandler} className="signButton" >Sign in (Admin)</button>
-                    </form>
-                    <form action="./User">
-                        <button onClick={this.logTheStateHandler} className="signButton" >Sign in (User)</button>
+                    <input type="Email" placeholder="Email" onChange={this.emailTypedHandler} required></input>
+                    <input type="Password" placeholder="Password" onChange={this.passwordTypedHandler} required></input> 
+                    <form>
+                        <button onClick={this.logTheStateHandler} className="signButton" >Sign in</button>
                     </form>
                 </div>
             </div>
         );
     }
 }
+
+export default withRouter(SignInBox)
