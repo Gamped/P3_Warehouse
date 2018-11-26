b6db356112eb9593e9bdcdfa07fa72127564cfe9
Tobias <nielstns@gmail.com>
2018-11-26 09:07:20 +0100

    Started work on a textbox

---
+++ Frontend/src/components/MenuComponents/TextBox/TextBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+//gets and id and shows information in a box.
+
+    //display name in a h2.
+
+    //if client display company name and search for an adresse
+
+    //else if publisher still get the above, but also get a list of clients
+
+    // if order display Products, client, reciever adress, creation date, Title, 
+
+
+const TextBox = (props) =>{
+    const id = props.id;
+    const display;
+
+    return(
+        <div>
+            <h2></h2>
+
+        </div>
+    )
+}
+
+export default TextBox;
