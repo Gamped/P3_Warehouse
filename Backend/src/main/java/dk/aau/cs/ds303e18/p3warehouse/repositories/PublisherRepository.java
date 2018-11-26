f4585e82263e6dd65f101be2d031fb5c587d1a80
simonmartinnielsen <post.smn@gmail.com>
2018-11-26 15:14:20 +0100

    Working on controllers

--- Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/repositories/PublisherRepository.java
+++ Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/repositories/PublisherRepository.java
@@ -1,8 +1,23 @@
 package dk.aau.cs.ds303e18.p3warehouse.repositories;
 
+import dk.aau.cs.ds303e18.p3warehouse.models.restmodels.RestProductModel;
+import dk.aau.cs.ds303e18.p3warehouse.models.restmodels.RestPublisherModel;
 import dk.aau.cs.ds303e18.p3warehouse.models.users.Publisher;
+import dk.aau.cs.ds303e18.p3warehouse.models.warehouse.Product;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PublisherRepository extends MongoRepository<Publisher, ObjectId> {
+
+    List<Publisher> findAll();
+
+    Optional<Publisher> findById(@PathVariable String hexId);
+
+    void deleteById(String hexId);
+
 }
