c8b39381cf4b65edb62f7dcecd23dafc8f673c32
Tobias <nielstns@gmail.com>
2018-11-29 09:47:44 +0100

    Reverted minor changes from the revert

--- Frontend/src/components/Header/Header.js
+++ Frontend/src/components/Header/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import "./Header.css";
 
 // The header component
-<<<<<<< HEAD
 class Header extends React.Component {
  
     exitHandler = (e) =>{
@@ -31,15 +30,6 @@ const mapDispatchToProps = (dispatch) =>{
     return {
         login: (user) => {dispatch({type: "LOGOUT"})}
     }
-=======
-const Header = (props) => {
-    const title= "4N: " + props.title
-    return(
-        <div className="headerStyle">
-            <h1 className="headerText">{title}</h1>
-        </div>
-    );
->>>>>>> parent of 255b879... User is able to log on
 }
 
 export default Header;
