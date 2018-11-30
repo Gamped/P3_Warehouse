8b1d906e73b93780ab868b676f52c56a0cfd7c86
simonmartinnielsen <post.smn@gmail.com>
2018-11-30 13:07:41 +0100

    Conflicts

--- Frontend/src/components/pages/Admin/AdminOrders.js
+++ Frontend/src/components/pages/Admin/AdminOrders.js
@@ -3,8 +3,6 @@ import "../Pages.css";
 import "./AdminOrders.css";
 import axios from "axios";
 import ReactTable from "react-table";
-import 'react-table/react-table.css'
-import TextBox from "../../MenuComponents/TextBox/TextBox";
 import ButtonList from "../../MenuComponents/ButtonList/ButtonList";
 
 
@@ -36,54 +34,14 @@ export default class AdminOrders extends Component {
     this.makeButtonData = this.makeButtonData.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {     
-    var annotated=this.annotateOrders (nextProps.orders);
-     this.setState ({pipelines: annotated});      
-   } 
-
   componentDidMount() {
+
     axios.get("http://localhost:8080/api/orders")
     .then((response) => {
-        const data = this.makeData(response);
-        this.setState({ orders: data })
+        const orders = this.makeRow(response);
+        this.setState({ orders: orders })
       })
   }
-
-  annotatePipeline (orders) {
-    var annotated=new Array ();
-    
-    for (let i = 0; i < orders.length; i++) {
-      var converter = orders[i];
-      converter ["selected"]=false;
-      annotated.push (converter);
-    }
-    
-    return (annotated);
-  }
-
-
-  selectOrder (e,state,column,rowInfo,instance) {      
-    if (this.state.selectedIndex != -1) {
-      var previousSelectedOrder=this.state.orders[this.state.selectedIndex];
-      previousSelectedOrder.selected=false;
-    }
-    
-    let orders = this.state.orders[rowInfo.index];
-    orders.selected = true;
-    
-    if (orders != null) {
-      if (this.props.onClickOrder) {
-        this.props.onClickOrder(orders.orderId);  
-      } else {
-        console.log ("No event handler available to process pipeline select event");  
-      } 
-    } else {
-      console.log ("Error retrieving pipeline from pipeline list");  
-    }
-    
-    this.setState ({selectedIndex: rowInfo.index});
-  }
-
 
   makeButtonData(response) {
     var buttons = [];
@@ -99,7 +57,7 @@ export default class AdminOrders extends Component {
     return buttons;
   }
 
-  makeData(response) {
+  makeRow(response) {
 
     var orders = [];
 
@@ -111,21 +69,12 @@ export default class AdminOrders extends Component {
         quantity: order.orderline.quantity,
         packed: order.orderline.packed
       });
-
     });
     console.log(response.data[0]);
     console.log(orders);
     return orders;
   }
 
-  getColumns() {
-    return [
-        {Header: "Order Id", accessor: "orderId"},
-        {Header: "Owner", accessor: "owner"},
-        {Header: "Date", accessor: "date"},
-        {Header: "Items To Pack", accessor: "itemsToPack"},
-        {Header: "Packed?", accessor: "packed"}
-    ]
   changeList=(input)=>{
     this.setState({listShown: input});
   }
@@ -179,32 +128,4 @@ export default class AdminOrders extends Component {
             </div>
         )
     }
-    // update the state
-    this.setState({ selection });
-  }
-
-
-
-
-  render() {
-
-      let orders = <ReactTable data={this.state.orders} 
-                               columns={this.ordersTable} 
-                               className='-striped -highlight'
-                               defaultPageSize={10}
-                               getTdProps={(state, rowInfo, column, instance) => {
-                                 return {
-                                   onClick: e => {
-                                      this.selectOrder (e,state,column,rowInfo,instance)
-                                       }
-                                 }
-                               }
-     }/>
-       
-     return (  
-      <div>
-        {orders}
-      </div> );
-    
-  }
 }
