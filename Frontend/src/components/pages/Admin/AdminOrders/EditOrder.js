f623779635243ece0e0aaca7d6f845322302c0de
Martin <mjch17@student.aau.dk>
2018-12-06 14:24:51 +0100

    EditOrderContent Mostly done layoutwise

--- Frontend/src/components/pages/Admin/AdminOrders/EditOrder.js
+++ Frontend/src/components/pages/Admin/AdminOrders/EditOrder.js
@@ -22,6 +22,10 @@ export default class EditOrder extends Component{
         }
     }
 
+    sendToPage = (address) => {
+        this.props.history.push(address);
+    }
+
     render(){
         const orderLineColumns=[ /*You might wanna do something fancy where you can edit the quantity directly in the table*/
             {Header: "Products", accessor:"product"},
@@ -54,8 +58,8 @@ export default class EditOrder extends Component{
                         }}
                     />
                     <div className="Buttons container row">
-                        <button className="btn btn-warning btn-lg mx-2">Edit Address</button>
-                        <button className="btn btn-warning btn-lg mx-2">Edit Contents</button>
+                        <button className="btn btn-warning btn-lg mx-2" onClick={this.sendToPage("/Admin/Orders/Edit/OrderAddress")}>Edit Address</button>
+                        <button className="btn btn-warning btn-lg mx-2" onClick={this.sendToPage("/Admin/Orders/Edit/OrderContent")}>Edit Contents</button>
                     </div>
                 </div>
             </div>
