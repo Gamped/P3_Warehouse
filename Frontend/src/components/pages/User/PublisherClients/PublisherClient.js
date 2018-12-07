a8c79739900cbd4c8f429fea1442fe9d3f6fa7b3
Martin <mjch17@student.aau.dk>
2018-12-07 09:46:36 +0100

    Added a sweet table to PublisherClient

--- Frontend/src/components/pages/User/PublisherClients/PublisherClient.js
+++ Frontend/src/components/pages/User/PublisherClients/PublisherClient.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import "../../Pages.css";
-import "./PublisherClient.css";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import ReactTable from 'react-table';
+
+import "../../Pages.css";
+import "./PublisherClient.css";
 
 class PublisherClient extends React.Component {
     constructor(props) {
@@ -13,33 +15,50 @@ class PublisherClient extends React.Component {
     //Todo: get a pdf back when sending the userID
     //TODO: Indsæt et reactTable.
   render() {
+      const columns=[
+          {Header: "Product Name", accessor: "productName"},
+          {Header: "Product ID", accessor: "productId"},
+          {Header: "Quantity", accessor: "productQuantity"}
+      ]
         return(
             <div className="PageStyle rounded">
-                <nav className="navbar navbar-secondary bg-secondary"><h3> All clients stock</h3></nav>
-            
-                <div className="container row">
-                    <div className="stockListBox 'contentBoxStyle'">
-                        <table className="table">
-                            <tbody>
-                                <tr>
-                                    <th>Product name</th>
-                                    <th>Product Number</th>
-                                    <th>Quantity</th>
-                                </tr>
-                            </tbody>
-                        </table>
+                <nav className="navbar navbar-secondary bg-secondary"><h3> All clients' stock</h3></nav>
+                    <div className="mainContent container col">
+                        <ReactTable
+                            columns={columns}
+                            data={this.state.products}
+                            showPagination={false} 
+                            className="table -striped -highlight"
+                            getTrProps={(state, rowInfo) => {
+                                if (rowInfo && rowInfo.row) {
+                                    return {
+                                    onClick: (e) => {
+                                        
+                                        this.setState({selected: rowInfo.index, selectedId: rowInfo.original.hexId })
+                                        console.log(rowInfo.original)
+                                    },
+                                    style: {
+                                        background: rowInfo.index === this.state.selected ? '#00afec' : 'white',
+                                        color: rowInfo.index === this.state.selected ? 'white' : 'black'
+                                    }
+                                    }
+                                }else{
+                                    return {}
+                                }
+                            }}
+                        />
+
+                        <div className="buttons container row">
+                            <div className="col">    
+                                <Link to="/User/Clients/Request" className="btn btn-block btn-warning">Request client change</Link>
+                            </div>
+                            <div className="col">
+                                <button className="btn-info btn btn-block">Export their stock</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
   
                 
-                <div className="container row">
-                    <div className="col">    
-                        <Link to="/User/Clients/Request" className="btn btn-block btn-warning">Request client change</Link>
-                    </div>
-                    <div className="col">
-                        <button className="btn-info btn btn-block">Export their stock</button>
-                    </div>
-                </div>
             </div>
             
         );
