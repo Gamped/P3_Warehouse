be6d10ef357cccd3caf3c570293a7d804db0b7c5
Tobias <nielstns@gmail.com>
2018-12-07 09:20:07 +0100

    STop

--- Frontend/src/components/pages/User/UserStock/UserStock.js
+++ Frontend/src/components/pages/User/UserStock/UserStock.js
@@ -11,37 +11,21 @@ export default class UserStock extends React.Component {
         };
     }
 
-
-    /*
-    * SOME FUNCTION TO RETRIEVE & SEND INFO FROM DB
-    */
-
-    handleQuarry = (event) => {
-        this.setState({
-            quarry: event.target.value,
-        });
-    }
+    //TODO: INDSÆT REACT TABLE!
 
     render(){
         return(
             <div className="PageStyle rounded">
-                <div className="topBox topBoxStyle">
-                    <h2 className="topText text-center text-white"> Stock:</h2>
-                </div>
+                <navbar className="navbar navbar-secondary bg-secondary"><h2>Your Stock</h2></navbar>
 
-                <div className="stockDeciderBox bottomBoxStyle">
-                    <input 
-                        type="text" 
-                        className="serachBar" 
-                        onChange={this.handleQuarry}
-                        placeholder="Search for product(s)"/>
-                    <button className="stockExportButton stockButton_f btn">Export current list</button>
-                </div>
-
-                <div className="listBox contentBoxStyle">
-
-                </div>
+             
             </div>
         );
+    }
+}
+
+const mapStateToProps = (state) =>{
+    return{
+        userId: state.loginReducer.userId
     }
 }
