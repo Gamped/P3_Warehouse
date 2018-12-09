7f2b56acc07f47d6ee934c2432601b08dddd4b04
Chrissermand <chartv17@student.aau.dk>
2018-12-09 10:34:31 +0100

    Frontend EditAddres update

--- Frontend/src/components/pages/Admin/AdminOrders/EditOrderAddress.js
+++ Frontend/src/components/pages/Admin/AdminOrders/EditOrderAddress.js
@@ -7,7 +7,7 @@ import "./EditOrderAddress.css"
 export default class EditOrderAddress extends Component{
     constructor(props){
         super(props);
-        const {company, contactPerson, address, zipCode, city} = props;
+        const {company, cvr, contactPerson, phoneNumber, address, zipCode, city, country} = props;
         this.state = {
             
         }
@@ -16,23 +16,23 @@ export default class EditOrderAddress extends Component{
     render(){
         return(
             <div className="PageStyle rounded">
-                <div className="container col md-auto">
-                    <form>
+                    <div class="btn-group-vertical my-5 mx-5">
                         <input type="text" name="company" defaultValue={this.company} className="newForm" onChange={this.onChange} placeholder="Company"/> 
+                        <input type="text" name="cvr" defaultValue={this.cvr} className="newForm" onChange={this.onChange} placeholder="CVR"/> 
                         <input type="text" name="contactPerson" defaultValue={this.contactPerson} className="newForm" onChange={this.onChange} placeholder="Contact Person"/>
+                        <input type="text" name="phonenumber" defaultValue={this.phoneNumber} className="newForm" onChange={this.onChange} placeholder="Phonenumber"/>
                         <input type="text" name="address" defaultValue={this.address} className="newForm" onChange={this.onChange} placeholder="Address"/>
                         <input type="text" name="zipCode" defaultValue={this.zipCode} className="newForm" onChange={this.onChange} placeholder="Zipcode"/>
                         <input type="text" name="city" defaultValue={this.city} className="newForm" onChange={this.onChange} placeholder="City"/>
-                    </form>
-                    <div class="w-100"></div>
-                    <div ></div>
-                    <div className="container row">
+                        <input type="text" name="country" defaultValue={this.country} className="newForm" onChange={this.onChange} placeholder="Country"/>
+
+                    <div className="btn-group my-2">
                         <button className="col btn btn-succes mx-2">Save Changes</button>
                         <button className="col btn btn-warning mx-2">Discard Changes</button>
                         <Link to="/Admin/Order" className="col btn btn-info mx-2 " role=" button" >Back</Link>
                     </div>
+                    </div>
                 </div>
-            </div>
         )
     }
 }
--- Frontend/src/components/pages/Admin/AdminOrders/EditOrderContent.js
+++ Frontend/src/components/pages/Admin/AdminOrders/EditOrderContent.js
@@ -45,7 +45,9 @@ export default class EditOrderContent extends Component{
                 <div className="container col">     
                     <div className="mainContent container row">
                     <div className=" col col-md-auto ">
-                    <a className="navnbar" >Custumare order</a>   
+                        <nav class="navbar navbar-light bg-light"> 
+                            <a className="navnbar" >Custumare order</a>
+                        </nav>   
                         <ReactTable 
                             data={this.state.orderLineData}
                             columns={orderLineColumns}
@@ -74,40 +76,42 @@ export default class EditOrderContent extends Component{
                              <div class="h-25"></div>   
                              <div class="btn-group-vertical">
                                 <div className="row my-5">
-                                    <button className="addButton btn btn-success  mx-1"> Add Product </button>
+                                    <button className=" addButton btn btn-success  mx-1"> Add Product </button>
                                     <button className=" removeButton btn btn-danger mx-1">Remove Product</button>
                                 </div>  
                              </div>
                         </div>
                         
                         <div className=" col col-md-auto">
-                        <a className="navnbar" >Publiser stock</a>
-                        <ReactTable 
-                            data={this.state.productData}
-                            columns={productColumns}
-                            className="Products"
-                            showPagination={false} 
-                            className="productTable -striped -highlight"
-                            getTrProps={(state, rowInfo) => {
-                                if (rowInfo && rowInfo.row) {
-                                    return {
-                                        onClick: (e) => {
-                                            this.setStateAsSelected(rowInfo);
-                                            this.showOrderLines(rowInfo);
-                                        },
-                                        style: {
-                                            background: rowInfo.index === this.state.productSelected ? '#00afec' : 'white',
-                                            color: rowInfo.index === this.state.productSelected ? 'white' : 'black'
+                            <nav class="navbar navbar-light bg-light">
+                                <h className="navnbar" >Publiser stock</h>
+                            </nav>
+                            <ReactTable 
+                                data={this.state.productData}
+                                columns={productColumns}
+                                className="Products"
+                                showPagination={false} 
+                                className="productTable -striped -highlight"
+                                getTrProps={(state, rowInfo) => {
+                                    if (rowInfo && rowInfo.row) {
+                                        return {
+                                            onClick: (e) => {
+                                                this.setStateAsSelected(rowInfo);
+                                                this.showOrderLines(rowInfo);
+                                            },
+                                            style: {
+                                                background: rowInfo.index === this.state.productSelected ? '#00afec' : 'white',
+                                                color: rowInfo.index === this.state.productSelected ? 'white' : 'black'
+                                            }
                                         }
+                                    }else{
+                                        return {}
                                     }
-                                }else{
-                                    return {}
-                                }
-                            }}
-                        />
-                       </div> 
+                                }}
+                            />
+                        </div> 
                     </div>
-                    <div className="finishingButtons container row">
+                    <div className="finishingButtons container row my-3">
                         <button className="col  btn btn-success mx-2">Save Content</button>
                         <button className="col  btn btn-danger mx-2">Discard Content</button>
                         <button className="col btn btn-info mx-2" onClick={()=>this.sendToPage("/Admin/Order")}>Back</button>                       
