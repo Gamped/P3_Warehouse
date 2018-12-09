8981d60573c29ad1d7206a0e313759b19f35218d
Tobias <nielstns@gmail.com>
2018-12-09 18:32:06 +0100

    PDF

--- Frontend/src/components/pages/Admin/AdminOrders/AdminOrders.js
+++ Frontend/src/components/pages/Admin/AdminOrders/AdminOrders.js
@@ -150,16 +150,35 @@ export default class AdminOrders extends Component {
     export = () =>{
         const pdfConverter = require('jspdf');
         const doc = new pdfConverter();
-        alert("HIV HVAD DER STÅR I ORDER UD. SNAK VENLIGST MED TOBIAS")
+
+        /* TODO: Find ud af hvad der skal skrives ind i pdfen.*/
+        let pdfXPlace = 20;
+        let pdfYPlace = 50;
         
-        /* TODO: Find ud af hvad der skal skrives ind i pdfen.
 
         doc.setFontSize(22);
-        doc.text(20,50,"Entire stock:");
+        doc.text(pdfXPlace,pdfYPlace,"Packlist:");
+        pdfYPlace +=5;
+        doc.line(pdfXPlace,pdfYPlace,175,pdfYPlace);
+        doc.setFontSize(15);
+        pdfYPlace +=8;
+        doc.text(pdfXPlace,pdfYPlace, "Order nummer: "+ "[Insert order number here]" )
+        pdfYPlace +=8;
+        doc.text(pdfXPlace,pdfYPlace, "Customer: "+ "[Insert Customer here]" )
+        pdfYPlace +=8;
+        doc.text(pdfXPlace,pdfYPlace,"Order date: "+"[Insert Date here]")
         doc.setFontSize(10);
-        let pdfXPlace = 25;
-        let pdfYPlace = 65;
-        let counter = 0;
+        pdfYPlace +=10
+
+        doc.setFontStyle("bold")
+        doc.text(pdfXPlace,pdfYPlace,"Item ID")
+        doc.text(pdfXPlace+50,pdfYPlace,"Item Name")
+        doc.text(pdfXPlace+100,pdfYPlace,"Quantity")
+        doc.text(pdfXPlace+140,pdfYPlace,"Packed?")
+        pdfYPlace +=2
+        doc.line(pdfXPlace,pdfYPlace,175,pdfYPlace);
+
+        /*let counter = 0;
         for (const key in elements){
             
             doc.text("Name: "+elements[key].productName,pdfXPlace,pdfYPlace);
@@ -170,15 +189,87 @@ export default class AdminOrders extends Component {
             if(counter%25===0){
                 doc.addPage()
             }
-        }
+        }*/
 
-        doc.save("EntireStock.pdf")
-*/
+        doc.save("PackList.pdf")
+
     }
 
     finishOrder = () =>{
-        //TODO: See above.
-        alert("Missing func. Her skal den printe en følge seddel og slette ordren.")
+
+        //TODO: DELETE ORDER TOO
+        const pdfConverter = require('jspdf');
+        const doc = new pdfConverter();
+
+        /* TODO: Find ud af hvad der skal skrives ind i pdfen.*/
+        let pdfXPlace = 20;
+        let pdfYPlace = 50;
+        
+        doc.line(pdfXPlace,pdfYPlace,175,pdfYPlace);
+
+        pdfYPlace += 7
+        doc.setFontSize(12)
+        doc.text(pdfXPlace,pdfYPlace,"Leveres til:")
+        doc.setFontSize(16)
+        doc.text(pdfXPlace+90, pdfYPlace,"Følgeseddel: "+"[INSERT NUMBER]")
+        
+        pdfYPlace += 5
+        doc.setFontSize(12)
+        doc.text(pdfXPlace,pdfYPlace,"[Company name]")
+        doc.text(pdfXPlace+90, pdfYPlace,"Ordernr.: "+"[INSERT NUMBER]")
+
+        pdfYPlace += 5
+        doc.text(pdfXPlace,pdfYPlace,"[Person]")
+        doc.text(pdfXPlace+90, pdfYPlace,"Dato.: "+"[INSERT Date]")
+
+        pdfYPlace += 5
+        doc.text(pdfXPlace,pdfYPlace,"[Address]")
+        doc.text(pdfXPlace+90, pdfYPlace,"Kundenr.: "+"[INSERT NUMBER]")
+
+        pdfYPlace += 5
+        doc.text(pdfXPlace,pdfYPlace,"[zip and city name]")
+        doc.text(pdfXPlace+90, pdfYPlace,"Sag: "+"[INSERT CASE]")
+
+        pdfYPlace += 5
+        doc.text(pdfXPlace,pdfYPlace,"[Country]")
+        doc.text(pdfXPlace+90, pdfYPlace,"Reference: "+"[INSERT SPECIFIC PERSON]")
+
+        pdfYPlace += 10
+
+        doc.text(pdfXPlace,pdfYPlace,"Hermed følger:")
+        pdfYPlace +=5
+
+        doc.text(pdfXPlace,pdfYPlace,"Titel: " + "[INSERT TITEL]")
+        doc.setFontSize(18)
+        doc.text(pdfXPlace+90,pdfYPlace,"SLUTLEVERING")
+        doc.setFontSize(12)
+        pdfYPlace +=5
+
+        doc.text(pdfXPlace,pdfYPlace,"Rekv.nr.: " +"[Insert number]")
+        pdfYPlace +=7
+
+        doc.setFontStyle("bold")
+        doc.text(pdfXPlace,pdfYPlace,"Item ID")
+        doc.text(pdfXPlace+50,pdfYPlace,"Item Name")
+        doc.text(pdfXPlace+140,pdfYPlace,"Quantity")
+        pdfYPlace +=2
+
+        doc.line(pdfXPlace,pdfYPlace,175,pdfYPlace);
+
+        /*let counter = 0;
+        for (const key in elements){
+            
+            doc.text("Name: "+elements[key].productName,pdfXPlace,pdfYPlace);
+            doc.text("Quantity: " + elements[key].quantity,pdfXPlace+120,pdfYPlace);
+            doc.line(20,pdfYPlace+5,175,pdfYPlace+5);
+            pdfYPlace += 17;
+            counter += 1;
+            if(counter%25===0){
+                doc.addPage()
+            }
+        }*/
+
+        doc.save("Følgeseddel.pdf")
     }
 
     render() {
