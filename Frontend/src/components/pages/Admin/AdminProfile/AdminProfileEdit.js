4fc7c8e6d27c1d1ec0fcefe35d3171713f031a32
Martin <mjch17@student.aau.dk>
2018-12-10 09:25:28 +0100

    AdminProfileEdit actually works now

--- Frontend/src/components/pages/Admin/AdminProfile/AdminProfileEdit.js
+++ Frontend/src/components/pages/Admin/AdminProfile/AdminProfileEdit.js
@@ -13,7 +13,7 @@ class AdminProfileEdit extends React.Component {
             userId: props.userId,
             nickname: "",
             passwordRepeat: "",
-            password: "",
+            password: ""
         };
     }
 
@@ -21,10 +21,14 @@ class AdminProfileEdit extends React.Component {
     confirmed = (event) =>{
         event.preventDefault();
         const {nickname, password, passwordRepeat} = this.state;
+        console.log(password);
+        console.log(passwordRepeat);
+        console.log(this.state.password);
+        console.log(this.state.passwordRepeat);
         
         if (password===passwordRepeat){
             Axios.put("http://localhost:8080/api/employee/edit/" + this.state.userId, 
-            {data: {nickname: nickname, password: password}});
+            {nickname: this.state.nickname, password: this.state.password});
             this.props.history.push("/Admin/Profile")
         }else{
             alert("Password has not been repeated correctly.")
@@ -61,16 +65,19 @@ class AdminProfileEdit extends React.Component {
                                 className="my-2 form-control" 
                                 onChange={this.handleName}
                                 placeholder="Name"/>
+                                {console.log(this.state.nickname)}
                             <input
                                 type="text" 
                                 className="my-2 form-control" 
                                 onChange={this.handlePass}
                                 placeholder="New password"/>
+                                {console.log(this.state.password)}
                             <input
                                 type="password" 
                                 className="my-2 form-control" 
-                                onChange={this.onChangeHandler}
+                                onChange={this.handlePassRepeat}
                                 placeholder="New password repeat"/>
+                                {console.log(this.state.passwordRepeat)}
                         </form>
 
                         <form className="newForm stockForm">
