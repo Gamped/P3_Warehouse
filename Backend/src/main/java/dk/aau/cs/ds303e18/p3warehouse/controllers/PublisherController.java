72c2ada768b24e6a06bba2442aacc5c252b20751
ScumbagDog <lbrack17@student.aau.dk>
2018-12-10 10:19:16 +0100

    Lavede order POST funktion om, stadig lidt wip

--- Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/controllers/PublisherController.java
+++ Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/controllers/PublisherController.java
@@ -1,10 +1,7 @@
 package dk.aau.cs.ds303e18.p3warehouse.controllers;
 
-import dk.aau.cs.ds303e18.p3warehouse.models.orders.Order;
 import dk.aau.cs.ds303e18.p3warehouse.models.restmodels.RestPublisherModel;
-import dk.aau.cs.ds303e18.p3warehouse.models.users.Client;
 import dk.aau.cs.ds303e18.p3warehouse.models.users.Publisher;
-import dk.aau.cs.ds303e18.p3warehouse.models.warehouse.Product;
 import dk.aau.cs.ds303e18.p3warehouse.repositories.OrderRepository;
 import dk.aau.cs.ds303e18.p3warehouse.repositories.ProductRepository;
 import dk.aau.cs.ds303e18.p3warehouse.repositories.PublisherRepository;
@@ -13,11 +10,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RequestMapping("/api")
 @CrossOrigin
