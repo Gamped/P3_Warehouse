0b665a8fb67e792f629a27f2f2faa0cf5eaa24dd
Tobias <nielstns@gmail.com>
2018-12-12 14:15:01 +0100

    Login Updated

--- Frontend/src/components/MenuComponents/Login/Login.js
+++ Frontend/src/components/MenuComponents/Login/Login.js
@@ -29,11 +29,10 @@ class SignInBox extends React.Component {
         get("users/login/" + this.state.userName + "/" +this.state.password,(res)=>{
             console.log(res)
             console.log(this.state)
-            
-            /*this.props.setUserName(res.nickName);
+            this.props.setUserName(res.contactInformation.nickName);
             this.props.setUserType(res.userType);
             this.props.setUserId(res.hexId)
-            this.props.setlogIn("True")*/
+            this.props.setlogIn("True")
         })
     }
 
