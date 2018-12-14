260ce1cced9ea80a7e8bd24215a3436849dac1dc
Tobias <nielstns@gmail.com>
2018-12-14 13:47:35 +0100

    Edit Order no longer crashes on load

--- Frontend/src/components/pages/Admin/AdminOrders/EditOrder.js
+++ Frontend/src/components/pages/Admin/AdminOrders/EditOrder.js
@@ -3,7 +3,7 @@ import ReactTable from 'react-table';
 import "../../Pages.css";
 import {connect} from "react-redux";
 import {getColumnsFromArray} from './../../../../handlers/columnsHandlers.js';
-import {get, put} from './../../../../handlers/requestHandlers.js';
+import {put} from './../../../../handlers/requestHandlers.js';
 import {makeOrderLinesData, makeCustomerProductsData} from './../../../../handlers/dataHandlers.js';
 import {amountIsNotANumberWarning, amountExceedingQuantityWarning} from './../../../../handlers/exceptions.js';
 import "./EditOrder.css";
@@ -98,26 +98,27 @@ class EditOrder extends Component{
     render() {
         console.log("State:",this.state)
         
-        let orderLines = [];
-        orderLines = this.props.orderLines
+        let newLines = [];
+        let orderLines = this.props.orderLines
         if(orderLines !== undefined){
             orderLines.forEach((orderLine) => {
                 orderLine.product.amount = orderLine.quantity;
             })
-            
+            orderLines.forEach((orderLine) => {
+                newLines.push(orderLine.product)
+            })
         }
         
         console.log("orderlines:",orderLines)
 
        let orderLineColumns = getColumnsFromArray(["Product Name", "Product Id", "Amount", "Quantity"]);
-        orderLineColumns[2].Cell = this.renderEditable;
         
         return (
              <div className="PageStyle customText_b">
                 <div className="frameBordering">
                     <div className="EditOrderLeft">
                     <ReactTable 
-                        data={orderLines}
+                        data={newLines}
                         columns={orderLineColumns}
                         showPagination={false}
                         className="editOrderColor -striped -highlight"
@@ -150,28 +151,28 @@ class EditOrder extends Component{
                             <div className="input-group-prepend">
                                 <label className="input-group-text" htmlFor="name" id="Item">Name</label> 
                             </div>
-                            <input id="name" className="form-control" type="text" name="name" onChange={()=>{}} defaultValue={this.state.selectedOrderLine.product.productName} placeholder="Name"/>
+                            <input id="name" className="form-control" type="text" name="name" onChange={()=>{}} defaultValue={this.state.selectedOrderLine.productName} placeholder="Name"/>
                         </div>
 
                         <div className="input-group my-2">
                             <div className="input-group-prepend">
                                 <label className="input-group-text" htmlFor="id" id="Item2">product Id</label> 
                             </div>
-                            <input id="id" className="form-control" type="text" name="id" onChange={()=>{}} defaultValue={this.state.selectedOrderLine.product.productId} placeholder="Product id"/>
+                            <input id="id" className="form-control" type="text" name="id" onChange={()=>{}} defaultValue={this.state.selectedOrderLine.productId} placeholder="Product id"/>
                         </div>
 
                         <div className="input-group my-2">
                             <div className="input-group-prepend">
                                 <label className="input-group-text" htmlFor="amount" id="Item3">Amount</label> 
                             </div>
-                            <input id="amount" className="form-control" type="number" name="amount" onChange={()=>{}} defaultValue={this.state.selectedOrderLine.quantity} placeholder="Amount ordered"/>
+                            <input id="amount" className="form-control" type="number" name="amount" onChange={()=>{}} defaultValue={this.state.selectedOrderLine.amount} placeholder="Amount ordered"/>
                         </div>
 
                         <div className="input-group my-2">
                             <div className="input-group-prepend">
                                 <label className="input-group-text" htmlFor="name" id="Item">Quantity in Stock: </label> 
                             </div>
-                            <label id="name" className="input-group-text" type="text"> {this.state.selectedOrderLine.product.quantity}</label>
+                            <label id="name" className="input-group-text" type="text"> {this.state.selectedOrderLine.quantity}</label>
                         </div>
 
                         </form>
