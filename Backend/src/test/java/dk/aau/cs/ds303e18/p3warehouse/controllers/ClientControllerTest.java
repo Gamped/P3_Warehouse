0d9ebeaf50bb8e91f9ba9ce94cb468801089f3f6
Martin <mjch17@student.aau.dk>
2018-12-16 21:31:53 +0100

    Removed an unneccessary test and updated another one to make a proper restmodel

--- Backend/src/test/java/dk/aau/cs/ds303e18/p3warehouse/controllers/ClientControllerTest.java
+++ Backend/src/test/java/dk/aau/cs/ds303e18/p3warehouse/controllers/ClientControllerTest.java
@@ -77,24 +77,12 @@ public class ClientControllerTest {
         clientRepository.save(client);
 
         RestClientModel restClientModel = new RestClientModel();
+        BeanUtils.copyProperties(client, restClientModel);
         restClientModel.setUserName("Bangerung");
 
         clientController.updateClient(client.getHexId(), restClientModel);
 
         assertEquals(clientRepository.findById(client.getHexId()).getUserName(), restClientModel.getUserName());
-    }
-
-    @Test
-    public void testUpdateClientCollateralDamage(){ //Currently failed due to tested method being sub-optimal
-        Client client = makeDummyClient(0);
-        clientRepository.save(client);
-
-        RestClientModel restClientModel = new RestClientModel();
-        restClientModel.setUserName("Bangerung");
-
-        clientController.updateClient(client.getHexId(), restClientModel);
-
-        assertEquals(clientRepository.findById(client.getHexId()).getPassword(), client.getPassword());
     }
 
     @Test
