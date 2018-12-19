eb613b8aa29e472700c0b5b8d3d986e34a9ac781
gamped <mathiasgam@gmail.com>
2018-12-19 11:41:12 +0100

    Code trim :)

--- Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/models/restmodels/RestEmployeeModel.java
+++ Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/models/restmodels/RestEmployeeModel.java
@@ -5,29 +5,17 @@ public class RestEmployeeModel extends RestUserModel{
     private String password;
     private String nickname;
 
-    public String getUserName() {
-        return userName;
-    }
+    public String getUserName() {return userName;}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+    public void setUserName(String userName) {this.userName = userName;}
 
     @Override
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() {return password;}
 
     @Override
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) {this.password = password;}
 
-    public String getNickname() {
-        return nickname;
-    }
+    public String getNickname() {return nickname;}
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
+    public void setNickname(String nickname) {this.nickname = nickname;}
 }
--- Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/models/restmodels/RestOrderLineModel.java
+++ Backend/src/main/java/dk/aau/cs/ds303e18/p3warehouse/models/restmodels/RestOrderLineModel.java
@@ -1,7 +1,5 @@
 package dk.aau.cs.ds303e18.p3warehouse.models.restmodels;
 
-import dk.aau.cs.ds303e18.p3warehouse.models.warehouse.Product;
-
 public class RestOrderLineModel {
     private RestProductModel restProductModel;
     private int amount;
