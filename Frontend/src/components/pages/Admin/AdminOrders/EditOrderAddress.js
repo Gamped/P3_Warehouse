3724c455f4f73dd6a9334dcf912babc12c00c84e
Chrissermand <chartv17@student.aau.dk>
2018-12-19 18:18:03 +0100

    edit order address fix

--- Frontend/src/components/pages/Admin/AdminOrders/EditOrderAddress.js
+++ Frontend/src/components/pages/Admin/AdminOrders/EditOrderAddress.js
@@ -71,7 +71,7 @@ import { makeOrderBodyFromData } from '../../../../handlers/bodyHandlers';
                     <input type="text" name="country" defaultValue={this.state.order.country} className="my-2 form-control" onChange={this.onChange} placeholder="Country"/>
 
                     <button className="EOA_BTN AdinOrderButtonSizer btn yellow_BTN mx-2" onClick={this.onSubmit}>Save Changes</button>
-                    <Link to="/Admin/Orders " className="EOA_BTN AdinOrderButtonSizer btn std_BTN mx-2 " role=" button" >Back</Link>
+                    <Link to="/Admin/Orders" className="EOA_BTN AdinOrderButtonSizer btn std_BTN mx-2 " role=" button" >Back</Link>
                 </div>
             </div>
         )
