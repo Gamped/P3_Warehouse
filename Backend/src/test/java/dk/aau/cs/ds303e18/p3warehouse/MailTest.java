f2ddc576aa89efa55b2f193c0347e872daca2469
gamped <mathiasgam@gmail.com>
2018-12-19 20:01:14 +0100

    Rød grød med fløde

--- Backend/src/test/java/dk/aau/cs/ds303e18/p3warehouse/MailTest.java
+++ Backend/src/test/java/dk/aau/cs/ds303e18/p3warehouse/MailTest.java
@@ -6,7 +6,10 @@ public class MailTest {
 
     public static void main(String args[]){
 
+        // NOTE: You need a file called "mail.txt" on your desktop with the following: [gmail address];[password]
+        // The gmail account needs to allow "less secure apps"
+        // NOTE: You need to change the email to your own, in order to recieve a mail
         OrderInfoMail mailSender = new OrderInfoMail("A Java program");
-        mailSender.sendOrderMsg("TEST order", "mathiasgam@gmail.com");
+        mailSender.sendOrderMsg("TEST order", "mgampe17@student.aau.dk");
     }
 }
